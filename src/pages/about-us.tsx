70601f3ff420ba0b0a84a16d4d5cb8c95ac3d32e
sdfwefk11 <mknhsad3398@naver.com>
2023-07-24 19:17:01 +0900

    Link pages

---
+++ src/pages/about-us.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default function AboutUs() {
+  return (
+    <Layout title="About Us">
+      <p>Sticker store.</p>
+    </Layout>
+  );
+}
---
+++ src/pages/blog.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+function Blog() {
+  return (
+    <Layout title="Blog">
+      <p>Sticker store.</p>
+    </Layout>
+  );
+}
+export default Blog;
