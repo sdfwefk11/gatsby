93c7fcbc2ce91c1fa0bce59c07fa2f4eedc82fad
sdfwefk11 <mknhsad3398@naver.com>
2023-07-27 22:56:53 +0900

    blog page query

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,13 +1,28 @@
+import { graphql, PageProps } from "gatsby";
 import React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 
-export default function Blog() {
+export default function Blog({ data }: PageProps<Queries.BlogTitlesQuery>) {
+  console.log(data);
   return (
     <Layout title="Blog">
-      <p>Sticker store.</p>
+      <ul>
+        {data.allFile.nodes.map((file, index) => (
+          <li key={index}>{file.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 }
+export const query = graphql`
+  query BlogTitles {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Blog" />;
