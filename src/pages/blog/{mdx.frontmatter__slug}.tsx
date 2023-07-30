52f08b6317bb3f3e51aec422d07d5392fb7a3d22
sdfwefk11 <mknhsad3398@naver.com>
2023-07-30 23:37:21 +0900

    Mdx blog post

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,10 +1,37 @@
+import { graphql } from "gatsby";
 import React from "react";
 import Layout from "../../components/Layout";
+import Seo from "../../components/Seo";
 
-export default function BlogPost() {
+interface IBlogPostProps {
+  data: Queries.PostDetailQuery;
+  children: any;
+}
+
+export default function BlogPost({ data, children }: IBlogPostProps) {
+  console.log(children);
   return (
-    <Layout title="Blog Post">
-      <div />
+    <Layout title="">
+      <div>{children}</div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      body
+      frontmatter {
+        author
+        category
+        date
+        slug
+        title
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }: IBlogPostProps) => (
+  <Seo title={data.mdx?.frontmatter?.title} />
+);
